5845ef18cdf9ca579c005c95aef66f1682a0d909
ColorblindMonk <ColorblindMonk@gmail.com>
2022-02-12 18:08:29 -0600

    Import tailwindcss dependencies

---
+++ Assignment 1/tailwind.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    content: [
+        './public/**/*.html',
+        './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'bdazzled-blue': '#3D5A80',
+                'darksky-blue': '#98C1D9',
+                'light-cyan': '#E0FBFC',
+                'gunmetal': '#293241',
+                'burnt-sienna': '#EE6C4D',
+            },
+        },
+        fontFamily: {
+            poppins: ["Poppins, sans-serif"],
+        },
+        container: {
+            center: true,
+            padding: '3rem',
+        },
+    },
+    plugins: [
+        require('tailwindcss-textshadow'),
+    ]
+};
